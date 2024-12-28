e86d81c935b4547c14f65f65dbcd399fba62b0cf
jhoabran <69612177+jhoabran@users.noreply.github.com>
2024-12-28 01:01:30 -0400

    Added initial configuration and Storybook setup for rn-eventify

---
+++ app.config.js
@@ -0,0 +1,30 @@
+export default ({ config }) => ({
+  ...config,
+  name: "rn-eventify",
+  slug: "rn-eventify",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: {
+    storybookEnabled: process.env.STORYBOOK_ENABLED,
+  },
+});
