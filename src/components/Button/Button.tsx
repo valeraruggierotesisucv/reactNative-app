80af45a6cd3bff3c79144b2cfd80dcca38c884e0
jhoabran <jhoabran@gmail.com>
2025-01-01 15:18:45 -0400

    Refactor Button component for improved readability and add ProfileCard component with stories
    - Cleaned up formatting and improved code readability in Button component.
    - Introduced new ProfileCard component with props for user information and follow functionality.
    - Added stories for ProfileCard to showcase default and following states.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,67 +1,69 @@
-import { TouchableOpacity, Text} from "react-native"; 
+import { TouchableOpacity, Text } from "react-native";
 import { StyleSheet } from "react-native";
 import { theme } from "../../../utils/theme";
 
 export enum ButtonSize {
-    EXTRA_SMALL = "extraSmall", 
-    SMALL = "small", 
-    MEDIUM = "medium", 
-    LARGE = "large"
+  EXTRA_SMALL = "extraSmall",
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large",
 }
 
 export enum ButtonVariant {
-    PRIMARY = "primary"
+  PRIMARY = "primary",
 }
 
 interface ButtonProps {
-    label: string, 
-    onPress: () => void, 
-    size?: ButtonSize, 
-    variant?: ButtonVariant
+  label: string;
+  onPress?: () => void;
+  size?: ButtonSize;
+  variant?: ButtonVariant;
 }
 
 export function Button({
-    label, 
-    onPress, 
-    size = ButtonSize.MEDIUM, 
-    variant = ButtonVariant.PRIMARY
-}: ButtonProps){
-
-    return(
-        <TouchableOpacity style={[styles.container, styles[size], styles[variant]]} onPress={onPress}>
-            <Text style={styles.label}>{label}</Text>
-        </TouchableOpacity>
-    )
+  label,
+  onPress,
+  size = ButtonSize.MEDIUM,
+  variant = ButtonVariant.PRIMARY,
+}: ButtonProps) {
+  return (
+    <TouchableOpacity
+      style={[styles.container, styles[size], styles[variant]]}
+      onPress={onPress}
+    >
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        padding: 6, 
-    }, 
-    label: {
-        color: 'white'
-    }, 
-    extraSmall :{
-        borderRadius: 5, 
-        width: 72, 
-        height: 32
-    }, 
-    small: {
-        borderRadius: 5, 
-        width: 160, 
-        height: 32
-    }, 
-    medium:{
-        borderRadius: 30, 
-        width: 251
-    }, 
-    large: {
-        borderRadius: 30, 
-        width: 330
-    }, 
-    primary: {
-        backgroundColor: theme.colors['primary'], 
-    }
-})
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 6,
+  },
+  label: {
+    color: "white",
+  },
+  extraSmall: {
+    borderRadius: 5,
+    width: 72,
+    height: 32,
+  },
+  small: {
+    borderRadius: 5,
+    width: 160,
+    height: 32,
+  },
+  medium: {
+    borderRadius: 30,
+    width: 251,
+  },
+  large: {
+    borderRadius: 30,
+    width: 330,
+  },
+  primary: {
+    backgroundColor: theme.colors["primary"],
+  },
+});
