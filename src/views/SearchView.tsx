344fe0d9bd9f4ac3b60295b68fc70c8845adcc4b
ruggieroDaniela <chachy.drs@gmail.com>
2025-01-04 13:27:44 -0400

    searchView

--- src/views/SearchView.tsx
+++ src/views/SearchView.tsx
@@ -1,21 +1,105 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, Button } from "react-native";
+import { View, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { SearchStackNavigationProp } from "../navigators/SearchStack";
 import { SearchRoutes } from "../../utils/routes";
+import { EventCard } from "../components/EventCard/EventCard";
+import { AppHeader } from "../components/AppHeader/AppHeader";
+import { events } from "../../utils/dummyData";
+import { SearchBar } from "../components/SearchBar/SearchBar";
+import { useState } from "react";
+import { Tabs } from "../components/Tabs/Tabs";
+import { Pills } from "../components/Pills/Pills";
+
+export enum SearchTabsEnum  {
+  EVENTS = "Eventos", 
+  ACCOUNTS = "Cuentas"
+}
 
 export function SearchView() {
     const navigation = useNavigation<SearchStackNavigationProp>();
+    const [activeTab, setActiveTab] = useState<SearchTabsEnum>(SearchTabsEnum.EVENTS); 
+    const [search, setSearch] = useState(""); 
+
+    const searchTabs = [
+      { id: 'Eventos', label: SearchTabsEnum .EVENTS},
+      { id: 'Cuentas', label: SearchTabsEnum .ACCOUNTS },
+    ]
+  
+    const handleSearchChange = (text: string) => {
+      setSearch(text);
+    };
+
+    
     return(
-        <View style={{flex: 1, gap: 8}}>
-           <Text>SearchView</Text>
-            <Button
-                title="Ver detalles del evento"
-                onPress={() => navigation.navigate(SearchRoutes.EventDetails)}
+        <SafeAreaView style={styles.container}>   
+          <View style={styles.view}>
+            <AppHeader />
+            <SearchBar
+              placeholder="Search"
+              onChangeText={handleSearchChange}
+              value={search}
             />
-            <Button
-                title="Ver detalles del perfil"
-                onPress={() => navigation.navigate(SearchRoutes.ProfileDetails)}
+            <View style={styles.tabs}>
+              <Tabs 
+                tabs={[
+                  { id: 'Eventos', label: 'Eventos' },
+                  { id: 'Cuentas', label: 'Cuentas' },
+                ]}
+                onTabChange={(tabId) => console.log('Tab changed:', tabId)}
+              />
+            </View>
+            <View style={{padding: 5}}>
+              <Pills 
+                categories={[
+                  { id: 'all', label: 'All' },
+                  { id: 'concerts', label: 'Concerts' },
+                  { id: 'festivals', label: 'Festivals' },
+                  { id: 'clubs', label: 'Clubs' },
+                  { id: 'parties', label: 'Parties' },
+                ]}
+                onSelectCategories={(categoryIds) => console.log('Selected categories:', categoryIds)}
+              />
+            </View>            
+            
+            <FlatList
+              data={events}
+              renderItem={({ item }) => {
+                return(
+                  <EventCard 
+                    profileImage={item.profileImage}
+                    username={item.username}                
+                    eventImage={item.eventImage}
+                    title={item.title}
+                    description={item.description}
+                    isLiked={item.isLiked}
+                    date={item.date}
+                    onPressUser={() => navigation.navigate(SearchRoutes.EventDetails)}
+                    onComment={() => console.log("COMMENT")}
+                    onShare={() => console.log("SHARE")}
+                    onMoreDetails={() => navigation.navigate(SearchRoutes.EventDetails)}
+                  />
+                )
+              }}
+              contentContainerStyle={{ paddingBottom: 70 }}
             />
-        </View>
+          </View>   
+        </SafeAreaView>      
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  view: {
+    flexGrow: 1,
+    width: "100%",
+    paddingTop: 20,              // check padding 
+  },   
+  tabs: {
+    alignItems: "flex-start", 
+    paddingLeft: 5,
+    paddingBottom: 5
+  }
+});
