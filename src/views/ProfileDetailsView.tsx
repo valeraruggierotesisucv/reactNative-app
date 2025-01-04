4515c471d6725c18522757be5ba1688cbefbd250
ruggieroDaniela <chachy.drs@gmail.com>
2025-01-04 12:36:41 -0400

    ProfileDetails navigation

--- src/views/ProfileDetailsView.tsx
+++ src/views/ProfileDetailsView.tsx
@@ -1,9 +1,27 @@
-import { View, Text } from "react-native";
+import {SafeAreaView, ScrollView, StyleSheet} from "react-native";
+import { AppHeader } from "../components/AppHeader/AppHeader";
+import { ProfileStackNavigationProp } from "../navigators/ProfileStack";
+import { useNavigation } from "@react-navigation/native";
 
 export function ProfileDetailsView() {
+    const navigation = useNavigation<ProfileStackNavigationProp>();
     return(
-        <View>
-            <Text>ProfileDetailsView</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollViewContent}>
+                <AppHeader title="Detalles del Perfil" goBack={navigation.goBack} />
+                
+
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scrollViewContent: {
+        flexGrow: 1,
+        paddingTop: 20
+    },
+});
