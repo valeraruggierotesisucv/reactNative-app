a5ade72d7b7e822544a06ff2699a726aa646eef1
ruggieroDaniela <chachy.drs@gmail.com>
2025-01-10 20:06:55 -0400

    refactor uploadImage

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,1 +1,15 @@
-// storage services
+import { supabase } from "../lib/supabase";
+
+export const uploadImage = async (uri: string) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    const arrayBuffer = await new Response(blob).arrayBuffer();
+    const fileName = `${Date.now()}.jpg`;
+    const { error } = await supabase
+      .storage
+      .from('EventImages')
+      .upload(fileName, arrayBuffer, { contentType: 'image/jpeg', upsert: false });
+    if (error) {
+      console.error('Error uploading image: ', error);
+    }
+}
--- src/views/AddDefaultView.tsx
+++ src/views/AddDefaultView.tsx
@@ -24,6 +24,7 @@ import { useTranslation } from "../contexts/TranslationContext";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { truncateString } from "../../utils/formatString";
 import { supabase } from "../lib/supabase";
+import { uploadImage } from "../services/storage";
 export enum StepsEnum {
   DEFAULT = "default",
   DATE = "date",
@@ -90,21 +91,6 @@ export function AddDefaultView({
       // UploadFile
     };
     pickDocument();
-  }
-
-
-  const uploadImage = async (uri: string) => {
-    const response = await fetch(uri);
-    const blob = await response.blob();
-    const arrayBuffer = await new Response(blob).arrayBuffer();
-    const fileName = `${Date.now()}.jpg`;
-    const { error } = await supabase
-      .storage
-      .from('EventImages')
-      .upload(fileName, arrayBuffer, { contentType: 'image/jpeg', upsert: false });
-    if (error) {
-      console.error('Error uploading image: ', error);
-    }
   }
 
   const openCamera = async () => {
