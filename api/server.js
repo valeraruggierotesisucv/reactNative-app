c58d72db9cc198b3b847bf0d77eae73e1656f14b
ruggieroDaniela <chachy.drs@gmail.com>
2025-01-12 12:01:53 -0400

    testApi

--- api/server.js
+++ api/server.js
@@ -2,7 +2,9 @@ const authenticateUser = require("./authenticateUser.js");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { PrismaClient } = require("@prisma/client"); 
 
+const db = new PrismaClient(); 
 // Middleware para permitir CORS
 app.use(cors());
 
@@ -10,8 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 // Ejemplo: Endpoint de test para obtener usuarios
-app.get("/api/users", (req, res) => {
-  res.json({ users: ["Daniela", "Miguel", "Juan"] });
+app.get("/api/users", async(req, res) => {
+  const users = await db.user.findMany(); 
+  res.json(users);
 });
 
 // Ejemplo ruta protegida
@@ -20,6 +23,7 @@ app.get('/api/protected', authenticateUser, (req, res) => {
       message: `Hola,  ${req.user.email} Accediste a una ruta protegida.` 
   });
 });
+
 
 // Iniciar el servidor
 app.listen(5000, () => {
