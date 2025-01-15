0eac87e759c5af8fc38a063812e6febba7952e69
ruggieroDaniela <chachy.drs@gmail.com>
2025-01-15 19:15:03 -0400

    cleanUp

--- src/models/EventModel.ts
+++ src/models/EventModel.ts
@@ -1,12 +1,5 @@
 import { apiRequest } from "../../utils/apiRequest";
-import { getServer } from "../../utils/getServer";
-
 export class EventModel {
-    constructor(){
-
-    }
-
-    // POST api/events
     static async createEvent(token: string, event: object){
         const createdEvent = await apiRequest(
             "events", 
